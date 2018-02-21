0dba4bfba9ff4633f25ba364c107843ab420d3ad
John Michael <ducnm.john98@gmail.com>
2018-02-21 21:00:52 +0700

    Redirect to login page

--- routes/nextPage.js
+++ routes/nextPage.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/:name', function(req, res, next) {
-  res.render('nextPage', {mess: req.params.name});
-});
+// router.get('/:name', function(req, res, next) {
+//   res.render('nextPage', {mess: req.params.name});
+// });
 
 router.get('/', function(req, res, next) {
   if (req.isAuthenticated())
     res.render('nextPage', {mess: req.user.username});
-  else res.redirect('/');
+  else res.redirect('/login');
 });
 
 module.exports = router;
